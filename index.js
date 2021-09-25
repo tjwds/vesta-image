a935133cc5e844200729c5cb7c84f638a4a2355e
Joe Woods <code@joewoods.dev>
2021-09-24 20:52:28 -0400

    Use Rik's new algorithm

--- index.js
+++ index.js
@@ -6,6 +6,42 @@ const widths = [
 ];
 const heights = [348, 410, 474, 536, 600, 661];
 
+const decodeBoard = (string) => {
+  string.replaceAll("-", "+").replaceAll("_", "/");
+  string = atob(string);
+
+  let codes = [];
+
+  const iterator = string[Symbol.iterator]();
+  let queue = Array.from(iterator);
+
+  while (queue.length) {
+    const char = queue.shift().codePointAt(0);
+
+    if (char & 128) {
+      if (queue.length === 0) throw "underrun!";
+
+      const code = char ^ 128;
+      const run = queue.shift().codePointAt(0);
+
+      codes.push(...Array(run).fill(code));
+      continue;
+    }
+
+    codes.push(char);
+  }
+
+  if (codes.length !== 6 * 22) {
+    console.log(`wrong length for encoded board: ${codes.length}`);
+    return Array(6).fill(Array(22).fill(0));
+  }
+
+  const a = Array(6)
+    .fill(1)
+    .map((i) => codes.splice(0, 22));
+  return a;
+};
+
 const splitflapValues = async function (input) {
   const flattened = input.flat();
   const possible = new Set();
@@ -38,7 +74,13 @@ const splitflapValues = async function (input) {
 
 // node index.js '[[67,65,6,13,67,65,6,13,67,65,6,13,67,65,6,13,67,65,6,13,67,65],[6,13,67,65,6,13,67,65,6,13,67,65,6,13,67,65,6,13,67,65,6,13],[67,65,6,13,67,65,6,13,67,65,6,13,67,65,6,13,67,65,6,13,67,65],[6,13,67,65,6,13,67,65,6,13,67,65,6,13,67,65,6,13,67,65,6,13],[67,65,6,13,67,65,6,13,67,65,6,13,67,65,6,13,67,65,6,13,67,65],[6,13,67,65,6,13,67,65,6,13,67,65,6,13,67,65,6,13,67,65,6,13]]'
 
-const input = JSON.parse(process.argv[2]);
+let input;
+const stateString = process.argv[2];
+try {
+  input = JSON.parse(stateString);
+} catch (err) {
+  input = decodeBoard(stateString)
+}
 
 const execute = async function () {
   const values = await splitflapValues(input);
